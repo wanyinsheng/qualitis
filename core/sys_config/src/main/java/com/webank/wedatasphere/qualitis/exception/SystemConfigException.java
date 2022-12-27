0a356a26d660124e13ebcb23adf42357cdd2d2ca
wanyinsheng <wan_yinsheng@163.com>
2022-12-27 13:04:28 +0800

    Initial commit

---
+++ core/sys_config/src/main/java/com/webank/wedatasphere/qualitis/exception/SystemConfigException.java
@@ -0,0 +1,12 @@
+package com.webank.wedatasphere.qualitis.exception;
+
+/**
+ * @author howeye
+ */
+public class SystemConfigException extends Exception {
+
+	public SystemConfigException(String message) {
+
+		super(message);
+	}
+}
